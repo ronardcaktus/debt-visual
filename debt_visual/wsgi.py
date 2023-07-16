b5da23ee61fbcb4b34b45e3288400cfa83dc7eb6
ronardcaktus <rluna@caktusgroup.com>
2023-07-15 20:40:32 -0400

    Create country model

--- debt_visual/wsgi.py
+++ debt_visual/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debt_visual.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "debt_visual.settings")
 
 application = get_wsgi_application()
