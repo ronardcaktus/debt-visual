01a50eb0021897989ad451a47052a060b5159a0d
ronardcaktus <rluna@caktusgroup.com>
2023-07-29 22:09:04 -0400

    Improve import

--- apps/countries/models.py
+++ apps/countries/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from numerize import numerize
 
-from apps.utils import (Continents, Regions, dollar_format,
-                        format_country_or_contient, global_gdp,
-                        population_format, world_population)
+from apps import utils
 
 
 class Country(models.Model):
@@ -14,14 +12,14 @@ class Country(models.Model):
     internal_debt = models.DecimalField(max_digits=100, decimal_places=2)
     external_debt = models.DecimalField(max_digits=100, decimal_places=2)
     continent = models.CharField(
-        max_length=max(len(x) for x in Continents.values),
-        choices=Continents.choices,
+        max_length=max(len(x) for x in utils.Continents.values),
+        choices=utils.Continents.choices,
         default="",
         blank=True,
     )
     region = models.CharField(
-        max_length=max(len(x) for x in Regions.values),
-        choices=Regions.choices,
+        max_length=max(len(x) for x in utils.Regions.values),
+        choices=utils.Regions.choices,
         default="",
         blank=True,
     )
@@ -65,53 +63,55 @@ class Country(models.Model):
 
         if population_number >= 1_000_000_000_000:
             suffix = " trillion"
-            formatted_population = population_format.format(
+            formatted_population = utils.population_format.format(
                 round(population_number / 1_000_000_000_000, 1)
             )
         elif population_number >= 1_000_000_000:
             suffix = " billion"
-            formatted_population = population_format.format(
+            formatted_population = utils.population_format.format(
                 round(population_number / 1_000_000_000, 1)
             )
         elif population_number >= 1_000_000:
             suffix = " million"
-            formatted_population = population_format.format(
+            formatted_population = utils.population_format.format(
                 round(population_number / 1_000_000, 1)
             )
         elif population_number >= 1_000:
             suffix = " thousand"
-            formatted_population = population_format.format(
+            formatted_population = utils.population_format.format(
                 round(population_number / 1_000, 1)
             )
         else:
-            formatted_population = population_format.format(population_number)
+            formatted_population = utils.population_format.format(population_number)
 
         return f"{formatted_population}{suffix}"
 
     @property
     def internal_debt_per_citizen(self):
-        return dollar_format.format(self.internal_debt / self.population)
+        return utils.dollar_format.format(self.internal_debt / self.population)
 
     @property
     def external_debt_per_citizen(self):
-        return dollar_format.format(self.external_debt / self.population)
+        return utils.dollar_format.format(self.external_debt / self.population)
 
     @property
     def total_debt_per_citizen(self):
-        return dollar_format.format(self.total_debt_as_int / self.population)
+        return utils.dollar_format.format(self.total_debt_as_int / self.population)
 
     @property
     def debt_to_gpd_ratio(self):
-        return dollar_format.format(self.total_debt_as_int / self.gdp)
+        return utils.dollar_format.format(self.total_debt_as_int / self.gdp)
 
     @property
     def population_percentage_of_the_world(self):
-        country_vs_world_population = round(self.population / world_population, 4) * 100
+        country_vs_world_population = (
+            round(self.population / utils.world_population, 4) * 100
+        )
         return f"{str(country_vs_world_population)}%"
 
     @property
     def gdp_as_percentage_of_global_gdp(self):
-        return (self.gdp / global_gdp) * 100
+        return (self.gdp / utils.global_gdp) * 100
 
     @property
     def gdp_as_percentage_of_region(self):
@@ -124,8 +124,8 @@ class Country(models.Model):
 
     @property
     def continent_formatted(self):
-        return format_country_or_contient(self.continent)
+        return utils.format_country_or_contient(self.continent)
 
     @property
     def region_formatted(self):
-        return format_country_or_contient(self.region)
+        return utils.format_country_or_contient(self.region)
