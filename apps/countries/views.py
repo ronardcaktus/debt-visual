0dd735ff5a290094bbdfafb6b8ad24e610035f39
ronardcaktus <rluna@caktusgroup.com>
2023-08-05 20:17:16 -0400

    Handle country not found condition

--- apps/countries/views.py
+++ apps/countries/views.py
@@ -10,10 +10,10 @@ def index(request):
 
 def country_detail(request):
     query = request.GET.get("q")
-    if not query:
-        countries = ""
-    countries = Country.objects.filter(Q(name__icontains=query))
-    context = {
-        "countries": countries,
-    }
-    return render(request, "country/country_detail.html", context)
+    if query:
+        countries = Country.objects.filter(Q(name__icontains=query))
+        context = {"countries": countries, "query": query}
+        if countries.exists():
+            return render(request, "country/country_detail.html", context)
+        else:
+            return render(request, "country/country_not_found.html", context)
